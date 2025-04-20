d32a654857d372f3d3343ce377e01a5649262373
QueenGTPS <sepoigaming12@gmail.com>
2025-04-20 16:06:27 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,3 @@
-<script>
-var canvas = document.getElementById("myCanvas");
-    var stage = new createjs.Stage(canvas);
-
-    var w = canvas.width;
-    var h = canvas.height;
-
-    var text = new createjs.Text(
-      "the longer I'm with you\n" +
-      "the more I love you",
-      "bold 24px Arial", "#fff"
-    );
-
-    text.textAlign = "center";
-    text.x = w / 2;
-    text.y = h / 2 - text.getMeasuredLineHeight();
-
-    stage.addChild(text);
-    stage.update();
-<script>
+onload = () => {
+  document.body.classList.remove("container");
+};
